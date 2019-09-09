8d33c28f0504a21a22cc1e0173b03f0e6b8ceeeb
biloholovskyi <biloholovskyi@yandex.ru>
2019-09-10 00:21:19 +0300

    click effect in navbar

---
+++ src/components/navBar/clickEffect.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ClickBox = styled.div `
+  position: absolute;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background-color: #627E51;
+  opacity: ${props => props.status === "scale" ? "0" : props.status === true ? ".2" : "0"};
+  transition: all .3s ease-in-out;
+  top: 50%;
+  left: 50%;
+  margin-top: -75px;
+  margin-left: -75px;
+  transform: ${props => props.status === "scale" ? "scale(1)" : props.status ? "scale(1)" : "scale(0)"};
+`;
+
+const ClickEffect = ({state}) => {
+  return (
+    <ClickBox status={state}/>
+  )
+};
+
+export default ClickEffect;
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
+
+import ClickEffect from "./clickEffect";
 
 const NavBarWrapper = styled.div`
   position: fixed;
@@ -9,7 +11,6 @@ const NavBarWrapper = styled.div`
   bottom: 0;
   background: #FFFFFF;
   box-shadow: 0px -1px 0px rgba(0, 0, 0, 0.1);
-  padding: 9px 0 9px 0;
   display: flex;
   align-items: flex-end;
   z-index: 500;
@@ -18,6 +19,10 @@ const NavBarWrapper = styled.div`
 
 const NavBarLink = styled.div`
   width: 100%;
+  height: 62px;
+  position: relative;
+  overflow: hidden;
+  //border-radius: 20px;
   &:hover {
     cursor: pointer;
   }
@@ -28,6 +33,8 @@ const NavBarLink = styled.div`
     align-items: center;
     width: 100%;
     height: 100%;
+    position: relative;
+    z-index: 500;
     span {
       font-size: 25px;
       color: rgba(57, 55, 56, 0.56);
@@ -55,35 +62,87 @@ const Name = styled.div`
   color: rgba(57, 55, 56, 0.56);
 `;
 
-const NavBar = () => {
-  return (
-    <NavBarWrapper>
-      <NavBarLink>
-        <NavLink exact to="/" activeClassName="active">
-          <span className="icon-home"></span>
-          <Name className="name">Главный</Name>
-        </NavLink>
-      </NavBarLink>
-      <NavBarLink>
-        <NavLink exact to="/validation" activeClassName="active">
-          <span className="icon-validation"></span>
-          <Name className="name">Валидации</Name>
-        </NavLink>
-      </NavBarLink>
-      <NavBarLink>
-        <NavLink exact to="/notifanction" activeClassName="active">
-          <span className="icon-notifanction"></span>
-          <Name className="name">Уведомления</Name>
-        </NavLink>
-      </NavBarLink>
-      <NavBarLink>
-        <NavLink exact to="/more" activeClassName="active">
-          <span className="icon-more small"></span>
-          <Name className="name">Еще</Name>
-        </NavLink>
-      </NavBarLink>
-    </NavBarWrapper>
-  )
-};
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        home: false,
+        valid: false,
+        not: false,
+        more: false
+      }
+    }
+  }
+  runAnim = (el) => {
+    console.log(this.state.data[el]);
+    this.setState(({data}) => {
+      const newArr = {...data, [el]: true};
+      return {
+        data: newArr
+      }
+    })
+  };
 
-export default NavBar;
+  stopAnim = (el) => {
+    setTimeout(() => {
+      this.setState(({data}) => {
+        const newArr = {...data, [el]: "scale"};
+        return {
+          data: newArr
+        }
+      })
+    }, 150);
+    setTimeout(() => {
+      this.setState(({data}) => {
+        const newArr = {...data, [el]: false};
+        return {
+          data: newArr
+        }
+      })
+    }, 200);
+    setTimeout(() => {
+      this.setState(({data}) => {
+        const newArr = {...data, [el]: false};
+        return {
+          data: newArr
+        }
+      })
+    }, 250);
+  };
+
+  render() {
+    return (
+      <NavBarWrapper>
+        <NavBarLink onMouseDown={() => this.runAnim("home")} onMouseUp={() => this.stopAnim("home")}>
+          <ClickEffect state={this.state.data.home}/>
+          <NavLink exact to="/" activeClassName="active">
+            <span className="icon-home"></span>
+            <Name className="name">Главный</Name>
+          </NavLink>
+        </NavBarLink>
+        <NavBarLink onMouseDown={() => this.runAnim("valid")} onMouseUp={() => this.stopAnim("valid")}>
+          <ClickEffect state={this.state.data.valid}/>
+          <NavLink exact to="/validation" activeClassName="active">
+            <span className="icon-validation"></span>
+            <Name className="name">Валидации</Name>
+          </NavLink>
+        </NavBarLink>
+        <NavBarLink onMouseDown={() => this.runAnim("not")} onMouseUp={() => this.stopAnim("not")}>
+          <ClickEffect state={this.state.data.not}/>
+          <NavLink exact to="/notifanction" activeClassName="active">
+            <span className="icon-notifanction"></span>
+            <Name className="name">Уведомления</Name>
+          </NavLink>
+        </NavBarLink>
+        <NavBarLink onMouseDown={() => this.runAnim("more")} onMouseUp={() => this.stopAnim("more")}>
+          <ClickEffect state={this.state.data.more}/>
+          <NavLink exact to="/more" activeClassName="active">
+            <span className="icon-more small"></span>
+            <Name className="name">Еще</Name>
+          </NavLink>
+        </NavBarLink>
+      </NavBarWrapper>
+    )
+  }
+};
